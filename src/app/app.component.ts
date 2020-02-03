70b1e48a7ac79979e708c2b1bb4d7db26adf0c1f
Tzu-Chi Yen <tzuchi.yen@colorado.edu>
2020-02-02 23:00:17 -0700

    updated arXiv link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { Title } from '@angular/platform-browser';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {Title} from '@angular/platform-browser';
+import {HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,9 @@ import { Title } from '@angular/platform-browser';
 export class AppComponent {
   title = 'app';
   isCollapsed = false;
+  screenHeight: number;
+  // screenWidth: number;
+
   logoClick = function () {
     // this.router.navigateByUrl('/network/fish');
     document.querySelectorAll('li.ant-menu-item-selected').forEach(function (e) {
@@ -18,11 +22,11 @@ export class AppComponent {
     });
   };
 
-  public setTitle( newTitle: string) {
-    this.titleService.setTitle( newTitle );
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
   }
 
-  getTitleFromRouter = function( router: Router) {
+  getTitleFromRouter = function (router: Router) {
     for (const r of router.config) {
       if ('/' + r.path === router.url) {
         return r.data ? r.data.title : 'Tzu-Chi Yen';
@@ -31,10 +35,19 @@ export class AppComponent {
   };
 
   constructor(private router: Router, private titleService: Title) {
+    this.getScreenSize();
     router.events.subscribe((event) => {  // fires on every URL change
       console.log(router);
       this.setTitle(this.getTitleFromRouter(router));
     });
+  }
+
+  // https://stackoverflow.com/questions/39888768
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+    this.screenHeight = window.innerHeight - 555 - 64 - 70;
+    // this.screenWidth = window.innerWidth;
+    // console.log(this.screenHeight, this.screenWidth);
   }
 }
 
