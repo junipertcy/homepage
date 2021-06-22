0fafec94739d4bd9eb6848ed369c03688dac4b85
Tzu-Chi Yen <tzuchi.yen@colorado.edu>
2021-06-22 07:32:44 -0600

    misc updates

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 `homepage` hosts the source code behind Tzu-Chi Yen's professional [website](https://junipertcy.info).
 
 The site is served by an [Amazon S3 bucket](https://aws.amazon.com/s3/).
+
+* Learn Angular on [MDN Web Docs](https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Angular_getting_started).
--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -17,6 +17,20 @@ export class NewsComponent {
   faGithub = faGithub;
   faTwitter = faTwitter;
 
+
+  // URLs
+  wiki_stat_inf = 'https://en.wikipedia.org/wiki/Statistical_inference';
+  wiki_tda = 'https://en.wikipedia.org/wiki/Topological_data_analysis';
+  wiki_comp_sys = 'https://en.wikipedia.org/wiki/Complex_system';
+  wiki_css = 'https://en.wikipedia.org/wiki/Computational_social_science';
+
+  ssd_eff = 'https://ssd.eff.org/en/module/communicating-others';
+
+  acda21 = 'https://www.siam.org/conferences/cm/conference/acda21';
+  isit21 = 'https://2021.ieee-isit.org/';
+  networks21 = 'https://networks2021.net/';
+
+
   constructor() {
   }
 }
